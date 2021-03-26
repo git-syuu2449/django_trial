dd0faa1b5e02194fca0487a527e621ba1a35fbc0
syuu2449 <o>
2021-03-26 15:31:49 +0900

    基本的なビューの追加

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,22 @@
 from django.urls import path
 from . import views
 
+# path() 関数は4つの引数を受け取ります。引数のうち route と view の2つは必須で、kwargs、name の2つは省略可能
+# urlpatterns = [
+#     # ex: /polls/
+#     path('', views.index, name='index'),
+#     # ex: /polls/5/
+#     path('<int:question_id>/', views.detail, name='detail'),
+#     # ex: /polls/5/results/
+#     path('<int:question_id>/results/', views.results, name='results'),
+#     # ex: /polls/5/vote/
+#     path('<int:question_id>/vote/', views.vote, name='vote'),
+# ]
+
+app_name = 'polls'
 urlpatterns = [
-    # path() 関数は4つの引数を受け取ります。引数のうち route と view の2つは必須で、kwargs、name の2つは省略可能
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
